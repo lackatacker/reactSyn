a47aef382ce2a635d043c2f1df41ef108e2a6b84
lackatacker <80588946+lackatacker@users.noreply.github.com>
2022-01-12 21:14:00 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+import MyStack from './components/mystack';
+
+export default function App() {
+  return (
+    <View style={{flex:1}}>
+        <MyStack />
+    </View>
+  );
+}
+
